a10ef96c77dfa4bc8531c50bd48c75e3f552db72
Thomas Raballand <TRA3561@INV030755.viseo.net>
2017-08-24 10:53:56 +0200

    Commit of the Shared Core 2.0 Class Library and UWP

---
+++ NetStandard2POC/NetStandard2Core/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace NetStandard2Core
+{
+    public class TestClass
+    {
+        public HtmlDocument TestMethod()
+        {
+            var url = "http://html-agility-pack.net/";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            return doc;
+        }
+    }
+}
